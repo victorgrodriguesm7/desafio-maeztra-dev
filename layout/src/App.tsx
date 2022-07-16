e84d6b51011c1247e988b6356fb45f2a7eccda53
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-07-16 11:30:16 -0300

    BannerPrincipal Adicionado ao Site

--- layout/src/App.tsx
+++ layout/src/App.tsx
@@ -1,10 +1,14 @@
 import { Header, PreHeader } from "./components"
+import MainBanner from "./components/MainBanner"
 
 function App() {
   return (
     <div>
       <PreHeader/>
       <Header/>
+      <main>
+        <MainBanner/>
+      </main>
     </div>
   )
 }
