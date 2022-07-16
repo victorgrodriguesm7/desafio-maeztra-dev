da383839ba5ee23287ad157cd89c4f34c0146306
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-07-16 09:48:03 -0300

    Criado Estilo global

---
+++ layout/src/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Titillium Web', sans-serif;
+  }
+  body, html {
+    background: #FAFAFA;
+    overflow: hidden;
+  }
+  @media(max-width: 1080px){
+    html {
+        font-size: 93.75%;
+    }
+  }
+  @media(max-width: 1024px){
+    html {
+        font-size: 87.5%;
+    }
+  }
+  @media(max-width: 720px){
+      html {
+          font-size: 80%;
+      }
+  }
+`;
+
+export { GlobalStyle };
