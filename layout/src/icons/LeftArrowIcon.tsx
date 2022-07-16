f338c9689bbdb4c047b89093adb3e7cb9a11e475
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-07-16 11:29:33 -0300

    Adicionado Icones para o Slider

---
+++ layout/src/icons/LeftArrowIcon.tsx
@@ -0,0 +1,18 @@
+function LeftArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="32"
+      fill="none"
+      viewBox="0 0 20 32"
+    >
+      <path
+        fill="#353535"
+        d="M.515 14.756L14.757.515a1.759 1.759 0 012.487 0l1.661 1.661c.686.686.687 1.797.003 2.484L7.621 16l11.286 11.34a1.758 1.758 0 01-.003 2.484l-1.66 1.661c-.687.687-1.801.687-2.488 0L.515 17.243a1.759 1.759 0 010-2.487z"
+      ></path>
+    </svg>
+  );
+}
+
+export default LeftArrowIcon;
---
+++ layout/src/icons/RightArrowIcon.tsx
@@ -0,0 +1,18 @@
+function RightArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="32"
+      fill="none"
+      viewBox="0 0 20 32"
+    >
+      <path
+        fill="#353535"
+        d="M18.905 17.244L4.663 31.484c-.687.688-1.8.688-2.487 0l-1.66-1.66a1.759 1.759 0 01-.004-2.484L11.8 16 .512 4.66a1.759 1.759 0 01.003-2.484L2.176.516a1.759 1.759 0 012.487 0l14.242 14.24c.686.687.686 1.8 0 2.488z"
+      ></path>
+    </svg>
+  );
+}
+
+export default RightArrowIcon;
