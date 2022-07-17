6bd89f1423e550861da6251f1c2170cda340f88f
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-07-17 18:27:02 -0300

    Adicionado BorderRadius como atributo ao Componente do botão

--- layout/src/components/Button/index.tsx
+++ layout/src/components/Button/index.tsx
@@ -5,12 +5,14 @@ type Props = React.HTMLAttributes<HTMLButtonElement> & CustomButtonProps;
 export default function Button({
   size = "normal",
   weight = 400,
+  borderRadius,
   ...props
 }: Props) {
   return (
     <CustomButton 
       size={size} 
       weight={weight} 
+      borderRadius={borderRadius}
       {...props}/>
   )
 }
--- layout/src/components/Button/style.ts
+++ layout/src/components/Button/style.ts
@@ -4,11 +4,12 @@ export interface CustomButtonProps {
     weight?: 400 | 700;
     size?: "small" | "normal";
     customFamily?: string;
+    borderRadius?: number;
 }
 
 export const CustomButton = styled.button<CustomButtonProps>`
     background: #FAA500;
-    border-radius: 8px;
+    border-radius: ${({ borderRadius }) => borderRadius != undefined ? borderRadius : 8}px;
     color: #FFF;
 
     font-size: ${({ size }) => size === "small" ? ".875" : "1"}rem;
