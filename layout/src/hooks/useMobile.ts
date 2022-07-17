29436600270b788fe763171c65188fc4c198e2de
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-07-17 18:33:58 -0300

    Criado Hook para responsividade

---
+++ layout/src/hooks/useMobile.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useMobile = () => {
+    const [width, setWidth] = useState<number>(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+    
+    const isMobile = width <= 774;
+
+    return {
+        isMobile
+    };
+}
+
+export default useMobile;
